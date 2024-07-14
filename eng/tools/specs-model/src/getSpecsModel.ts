662a59a20f8a35c8a8011c4988a4bcb758c2c05a
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-14 01:54:36 -0700

    ongoing

---
+++ eng/tools/specs-model/src/getSpecsModel.ts
@@ -0,0 +1,3 @@
+export async function getSpecsModel(path: string): Promise<string> {
+  return 'stub getSpecsModel'
+}
--- eng/tools/specs-model/src/index.ts
+++ eng/tools/specs-model/src/index.ts
@@ -1,4 +1,31 @@
+import { exit } from 'process'
+import { getSpecsModel } from './getSpecsModel.js'
+
+function getUsage(): string {
+  return (
+    '  Usage: npx get-specs-model <path-to-specs-file-or-directory>\n' +
+    'Returns: JSON metadata for the input file or directory.\n' +
+    '\n' +
+    'The input path:\n' +
+    '- Must be an absolute or relative path to local clone of the https://github.com/Azure/azure-rest-api-specs or https://github.com/Azure/azure-rest-api-specs-pr repo.\n' +
+    '- Must point to the <local_clone>/specification directory or one of its descendants.\n' +
+    '\n' +
+    'Example: npx get-specs-model $HOME/repos/azure-rest-api-specs/specification\n' +
+    "Returns: JSON with metadata for the entire 'specification' directory o the local clone of 'azure-rest-api-specs' repo.\n"
+  )
+}
+
 export async function main() {
-    console.log('blah')
+  const args: string[] = process.argv.slice(1)
+
+  if (args.length === 1) {
+    const path: string = args[0]!
+    const specsModel: any = await getSpecsModel(path)
+    console.log(JSON.stringify(specsModel))
+    exit(0)
+  } else {
+    console.error(getUsage())
+    exit(1)
+  }
 }
 
