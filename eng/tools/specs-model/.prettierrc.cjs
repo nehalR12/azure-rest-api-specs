71f5142164d918ab6ee345b042fff130aa21b089
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-14 01:37:36 -0700

    add basics

---
+++ eng/tools/specs-model/.prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  semi: false, // No semicolons.
+  singleQuote: true,
+  endOfLine: 'crlf',
+  printWidth: 80,
+}
+
+module.exports = config
